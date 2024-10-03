3a69091a8693966de09d91435ab4d5120d4e546a
박준현 <millkk04@naver.com>
2024-10-03 22:58:11 +0900

    Add Selecting_movies directory

---
+++ Selecting_movies/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MovieList from './MovieList'; // 방금 만든 MovieList 컴포넌트 가져오기
+
+function App() {
+  return (
+    <div className="App">
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
